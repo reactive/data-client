486cde831f9fa1131064da97bc8eeaa9dcc25445
Nathaniel Tucker <natmaster@gmail.com>
2019-04-11 15:38:35 -0700

    [feature] Janky SSR interface added to RestProvider (#41)
    Interface WILL change in 2.0. Not recommended for public consumption ATM.

--- src/react-integration/provider/index.tsx
+++ src/react-integration/provider/index.tsx
@@ -1,21 +1,30 @@
 import React, { ReactNode, useEffect } from 'react';
 import { StateContext, DispatchContext } from '../context';
-import masterReducer, { initialState } from '../../state/reducer';
+import masterReducer, {
+  initialState as defaultState,
+} from '../../state/reducer';
 import NetworkManager from '../../state/NetworkManager';
 import SubscriptionManager from '../../state/SubscriptionManager';
 import PollingSubscription from '../../state/PollingSubscription';
 import createEnhancedReducerHook from './middleware';
+import { State } from '../../types';
 
 interface ProviderProps {
   children: ReactNode;
   manager?: NetworkManager;
   subscriptionManager?: SubscriptionManager<any>;
+  initialState?: State<unknown>;
+  store?: {
+    state: State<unknown>;
+  };
 }
 /** Controller managing state of the REST cache and coordinating network requests. */
 export default function RestProvider({
   children,
   manager = new NetworkManager(),
   subscriptionManager = new SubscriptionManager(PollingSubscription),
+  initialState = defaultState,
+  store,
 }: ProviderProps) {
   // TODO: option to use redux
   const useEnhancedReducer = createEnhancedReducerHook(
@@ -23,6 +32,9 @@ export default function RestProvider({
     subscriptionManager.getMiddleware(),
   );
   const [state, dispatch] = useEnhancedReducer(masterReducer, initialState);
+  if (store) {
+    store.state = state;
+  }
 
   // if we change out the manager we need to make sure it has no hanging async
   useEffect(() => {
