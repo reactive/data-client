f7107b849cad064156627fb99a3a4919c5c9157f
Nathaniel Tucker <me@ntucker.me>
2019-04-21 01:46:29 -0700

    [docs] Get rid of cheesy copy

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -154,7 +154,6 @@ class Index extends React.Component {
       return (
         <div className="productShowcaseSection paddingBottom">
           <h2>Who is Using This?</h2>
-          <p>This project is used by all these people</p>
           <div className="logos">{showcase}</div>
           <div className="more-users">
             <a className="button" href={pageUrl('users.html')}>
