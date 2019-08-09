f18ca97171c5f20837b27807d115b52fa581a025
Nathaniel Tucker <me@ntucker.me>
2019-08-09 11:59:31 -0700

    [enhance] Point to test guide if test enviroment is detected with no Provider

--- src/react-integration/context.ts
+++ src/react-integration/context.ts
@@ -8,5 +8,8 @@ export const DispatchContext = React.createContext((value: ActionTypes) => {
     console.error(
       'It appears you are trying to use Rest Hooks without a provider.\nFollow instructions: https://resthooks.io/docs/getting-started/installation#add-provider-at-top-level-component',
     );
+    if (process.env.NODE_ENV === 'test') {
+      console.error('If you are trying to test: https://resthooks.io/docs/guides/unit-testing-hooks')
+    }
   }
 });
