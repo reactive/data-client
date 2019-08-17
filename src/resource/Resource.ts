7bd96d4b1aff67992de363b47133a44ad6908215
Leonardo Zizzamia <leonardo.zizzamia@coinbase.com>
2019-08-17 14:51:33 -0700

    [refactor] Abstracted isResponseEmpty conditional in a separate method

--- src/resource/Resource.ts
+++ src/resource/Resource.ts
@@ -3,7 +3,20 @@ import { Method } from '~/types';
 
 import SimpleResource from './SimpleResource';
 
-/** Represents an entity to be retrieved from a server. Typically 1:1 with a url endpoint. */
+const ResourceError = `JSON expected but not returned from API`;
+
+export const isResponseEmpty = (res: request.Response):boolean => {
+  const resIncludesJSON = res.type.includes('json');
+  // Empty is only valid when no response is expect (204)
+  const resNoResponse = res.text === '' && res.status === 204;
+  const resBodyEmpty = Object.keys(res.body).length === 0
+  return !(resIncludesJSON || resNoResponse) && resBodyEmpty;
+}
+
+/**
+ * Represents an entity to be retrieved from a server. 
+ * Typically 1:1 with a url endpoint. 
+ */
 export default abstract class Resource extends SimpleResource {
   /** A function to mutate all requests for fetch */
   static fetchPlugin?: request.Plugin;
@@ -19,15 +32,8 @@ export default abstract class Resource extends SimpleResource {
     if (this.fetchPlugin) req = req.use(this.fetchPlugin);
     if (body) req = req.send(body);
     return req.then(res => {
-      if (
-        !(
-          res.type.includes('json') ||
-          // empty is only valid when no response is expect (204)
-          (res.text === '' && res.status === 204)
-        ) &&
-        Object.keys(res.body).length === 0
-      ) {
-        throw new Error(`JSON expected but not returned from API`);
+      if (isResponseEmpty(res)) {
+        throw new Error(ResourceError);
       }
       return res.body;
     });
