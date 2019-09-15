3605552c85cee76cfcc391e9809cc5643d56442d
Nathaniel Tucker <me@ntucker.me>
2019-09-15 14:33:46 -0700

    [feature] Add useResourceNew() and useCacheNew()

--- src/react-integration/hooks/index.ts
+++ src/react-integration/hooks/index.ts
@@ -1,8 +1,10 @@
 import useFetcher from './useFetcher';
 import useCache from './useCache';
+import useCacheNew from './useCacheNew';
 import useResultCache from './useResultCache';
 import useRetrieve from './useRetrieve';
 import useResource from './useResource';
+import useResourceNew from './useResourceNew';
 import useSubscription from './useSubscription';
 import useMeta from './useMeta';
 import useError from './useError';
@@ -11,10 +13,12 @@ import useInvalidator from './useInvalidator';
 export {
   useFetcher,
   useCache,
+  useCacheNew,
   useError,
   useResultCache,
   useRetrieve,
   useResource,
+  useResourceNew,
   useSubscription,
   useMeta,
   useInvalidator,
--- src/react-integration/index.ts
+++ src/react-integration/index.ts
@@ -1,28 +1,9 @@
-import {
-  useCache,
-  useFetcher,
-  useRetrieve,
-  useResource,
-  useSubscription,
-  useResultCache,
-  useMeta,
-  useError,
-  useInvalidator,
-} from './hooks';
 import { CacheProvider, ExternalCacheProvider } from './provider';
 import NetworkErrorBoundary from './NetworkErrorBoundary';
 
+export * from './hooks';
 export * from './NetworkErrorBoundary';
 export {
-  useCache,
-  useFetcher,
-  useRetrieve,
-  useResource,
-  useSubscription,
-  useResultCache,
-  useInvalidator,
-  useMeta,
-  useError,
   CacheProvider,
   ExternalCacheProvider,
   NetworkErrorBoundary,
