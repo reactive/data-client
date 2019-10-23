06a3769bdee58d9bd959e186d46821dffe50be7a
Nathaniel Tucker <me@ntucker.me>
2019-10-22 19:14:09 -0700

    [internal] Get rid of outdated comments in useRetrieve()

--- src/react-integration/hooks/useRetrieve.ts
+++ src/react-integration/hooks/useRetrieve.ts
@@ -24,13 +24,11 @@ export default function useRetrieve<
   const fetch = useFetcher(fetchShape, true);
   const expiresAt = useExpiresAt(fetchShape, params);
 
-  // TODO: figure out how to express that body is optional in FetchShape as we don't need to cast here
   return useMemo(() => {
     if (Date.now() <= expiresAt) return;
     // null params mean don't do anything
     if (!params) return;
     return fetch(params);
-    // we don't care to re-request on body (should we?)
     // we need to check against serialized params, since params can change frequently
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [expiresAt, fetch, params && fetchShape.getFetchKey(params)]);
