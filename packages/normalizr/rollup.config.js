4b04b629e67cce295c82743cb6d3a6d9f99df506
Nathaniel Tucker <me@ntucker.me>
2020-01-28 20:26:30 -0800

    fix: ES export is non-min version

--- packages/normalizr/rollup.config.js
+++ packages/normalizr/rollup.config.js
@@ -1,9 +1,11 @@
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import filesize from 'rollup-plugin-filesize';
-import { name } from './package.json';
 import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
+
+import { name } from './package.json';
+
 const { presets } = require('./.babelrc');
 
 const isProduction = process.env.NODE_ENV === 'production';
@@ -16,30 +18,46 @@ function snakeCase(id) {
   return id.replace(/(-|\/)/g, '_').replace(/@/g, '');
 }
 
-export default [
-  {
+const configs = [];
+
+if (process.env.BROWSERSLIST_ENV === 'production') {
+  configs.push({
+    input: 'src/index.js',
+    output: [{ file: `${destBase}.es${destExtension}`, format: 'es' }],
+    plugins: [
+      babel({ runtimeHelpers: true, presets }),
+      isProduction && terser(),
+      filesize(),
+    ].filter(Boolean),
+  });
+} else {
+  configs.push({
     input: 'src/index.js',
     output: [
       { file: `${destBase}${destExtension}`, format: 'cjs' },
-      { file: `${destBase}.umd${destExtension}`, format: 'umd', name: snakeCase(name) },
-      { file: `${destBase}.amd${destExtension}`, format: 'amd', name: snakeCase(name) },
-      { file: `${destBase}.browser${destExtension}`, format: 'iife', name: snakeCase(name) }
+      {
+        file: `${destBase}.umd${destExtension}`,
+        format: 'umd',
+        name: snakeCase(name),
+      },
+      {
+        file: `${destBase}.amd${destExtension}`,
+        format: 'amd',
+        name: snakeCase(name),
+      },
+      {
+        file: `${destBase}.browser${destExtension}`,
+        format: 'iife',
+        name: snakeCase(name),
+      },
     ],
     plugins: [
       babel({ runtimeHelpers: true }),
       resolve({ extensions }),
       commonjs({ extensions }),
       isProduction && terser(),
-      filesize()
-    ].filter(Boolean)
-  },
-  {
-    input: 'src/index.js',
-    output: [{ file: `${destBase}.es${destExtension}`, format: 'es' }],
-    plugins: [
-      babel({ runtimeHelpers: true, presets }),
-      isProduction && terser(),
-      filesize()
-    ].filter(Boolean)
-  }
-];
+      filesize(),
+    ].filter(Boolean),
+  });
+}
+export default configs;
