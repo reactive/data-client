4aa1617c7f9ac739b204d54b4f4098a08a587801
Nathaniel Tucker <me@ntucker.me>
2020-12-29 20:05:27 -0800

    enhance: Remove FixtureManager in favor of MockResolver (#447)

--- packages/test/src/index.ts
+++ packages/test/src/index.ts
@@ -3,7 +3,6 @@ import { makeExternalCacheProvider, makeCacheProvider } from './providers';
 import MockProvider from './MockProvider';
 import mockInitialState from './mockState';
 export * from './managers';
-export { default as FixtureManager } from './FixtureManager';
 export { default as MockResolver } from './MockResolver';
 export type { Fixture, SuccessFixture, ErrorFixture } from './mockState';
 
