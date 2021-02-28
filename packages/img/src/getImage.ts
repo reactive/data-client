f504fb0c4098331807b47f2e14236220aa759437
Nathaniel Tucker <me@ntucker.me>
2021-02-28 17:08:32 -0600

    fix: Works in node

--- packages/img/src/getImage.ts
+++ packages/img/src/getImage.ts
@@ -1,5 +1,9 @@
 import { Endpoint, EndpointInstance } from '@rest-hooks/endpoint';
 
+function isNode() {
+  return process !== undefined && process.release.name === 'node';
+}
+
 const getImage: EndpointInstance<
   (this: EndpointInstance, { src }: { src: string }) => Promise<string>,
   string,
@@ -10,6 +14,8 @@ const getImage: EndpointInstance<
     { src }: { src: string },
   ): Promise<string> {
     return new Promise(resolve => {
+      // even if we polyfill, on server we don't want to actually wait to resolve the Image
+      if (isNode() || !Image) resolve(src);
       const img = new Image();
       img.onload = () => {
         resolve(src);
