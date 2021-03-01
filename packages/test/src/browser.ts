c29182a170b2e91fcf5d7f2cea9032eeee895d7d
Nathaniel Tucker <me@ntucker.me>
2021-02-28 23:06:56 -0600

    enhance: Define package exports for modern tooling

---
+++ packages/test/src/browser.ts
@@ -0,0 +1,6 @@
+import MockProvider from './MockProvider';
+import mockInitialState from './mockState';
+export { default as MockResolver } from './MockResolver';
+export type { Fixture, SuccessFixture, ErrorFixture } from './mockState';
+
+export { MockProvider, mockInitialState };
