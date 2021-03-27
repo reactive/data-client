9e3da94dfd5a0e13598d90829211481b2bf64614
Nathaniel Tucker <me@ntucker.me>
2021-03-27 18:40:18 -0500

    enhance: Throw error when urlRoot is not defined

--- packages/rest/src/SimpleResource.ts
+++ packages/rest/src/SimpleResource.ts
@@ -28,6 +28,16 @@ export default abstract class SimpleResource extends Entity {
 
   /** Returns the globally unique identifier for this SimpleResource */
   static get key(): string {
+    /* istanbul ignore else */
+    if (process.env.NODE_ENV !== 'production') {
+      if (this.urlRoot === undefined) {
+        throw new Error(`urlRoot is not defined for Resource "${this.name}"
+
+  Resources require a 'static urlRoot' or 'static get key()' defined.
+  (See https://resthooks.io/docs/api/resource#static-urlroot-string)
+`);
+      }
+    }
     return this.urlRoot;
   }
 
