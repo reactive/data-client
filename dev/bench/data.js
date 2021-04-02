cfec12e8800a9d15a4793890f4368f520e450e44
github-action-benchmark <github@users.noreply.github.com>
2021-04-02 06:23:47 +0000

    add Benchmark (benchmarkjs) benchmark result for f6e20f3c46350e2554fd15e5eefa0666341887a0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1617340730414,
+  "lastUpdate": 1617344627235,
   "repoUrl": "https://github.com/coinbase/rest-hooks",
   "entries": {
     "Benchmark": [
@@ -76,6 +76,44 @@ window.BENCHMARK_DATA = {
             "range": "±2.33%",
             "unit": "ops/sec",
             "extra": "55 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f6e20f3c46350e2554fd15e5eefa0666341887a0",
+          "message": "internal(pkg): bump @commitlint/config-conventional (#710)\n\nBumps [@commitlint/config-conventional](https://github.com/conventional-changelog/commitlint) from 12.0.1 to 12.1.1.\r\n- [Release notes](https://github.com/conventional-changelog/commitlint/releases)\r\n- [Changelog](https://github.com/conventional-changelog/commitlint/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/conventional-changelog/commitlint/compare/v12.0.1...v12.1.1)\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\n\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>\r\nCo-authored-by: Nathaniel Tucker <me@ntucker.me>",
+          "timestamp": "2021-04-01T23:21:54-07:00",
+          "tree_id": "c354d8fe9e4d773485806578a5864defad80dfec",
+          "url": "https://github.com/coinbase/rest-hooks/commit/f6e20f3c46350e2554fd15e5eefa0666341887a0"
+        },
+        "date": 1617344626148,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "normalizeLong",
+            "value": 72.6,
+            "range": "±1.35%",
+            "unit": "ops/sec",
+            "extra": "75 samples"
+          },
+          {
+            "name": "denormalizeLong",
+            "value": 28.24,
+            "range": "±3.06%",
+            "unit": "ops/sec",
+            "extra": "51 samples"
           }
         ]
       }
