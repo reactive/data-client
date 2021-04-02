1b88ff8693892f32623c7c7394f81200434efc8e
github-action-benchmark <github@users.noreply.github.com>
2021-04-02 05:18:50 +0000

    add Benchmark (benchmarkjs) benchmark result for f6253ead9d681f5f617e4ede2202253f5155467c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1617254008366,
+  "lastUpdate": 1617340730414,
   "repoUrl": "https://github.com/coinbase/rest-hooks",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,44 @@ window.BENCHMARK_DATA = {
             "range": "±2.96%",
             "unit": "ops/sec",
             "extra": "47 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f6253ead9d681f5f617e4ede2202253f5155467c",
+          "message": "internal(pkg): bump @anansi/babel-preset from 2.2.6 to 2.3.0 (#711)\n\nBumps [@anansi/babel-preset](https://github.com/ntucker/anansi/tree/HEAD/packages/babel-preset-anansi) from 2.2.6 to 2.3.0.\r\n- [Release notes](https://github.com/ntucker/anansi/releases)\r\n- [Changelog](https://github.com/ntucker/anansi/blob/master/packages/babel-preset-anansi/CHANGELOG.md)\r\n- [Commits](https://github.com/ntucker/anansi/commits/@anansi/babel-preset@2.3.0/packages/babel-preset-anansi)\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\n\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2021-04-01T22:17:16-07:00",
+          "tree_id": "dd071b1904cb2e03f5d23a6f8fb4a9abcd03541c",
+          "url": "https://github.com/coinbase/rest-hooks/commit/f6253ead9d681f5f617e4ede2202253f5155467c"
+        },
+        "date": 1617340729460,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "normalizeLong",
+            "value": 81.64,
+            "range": "±2.22%",
+            "unit": "ops/sec",
+            "extra": "71 samples"
+          },
+          {
+            "name": "denormalizeLong",
+            "value": 30.58,
+            "range": "±2.33%",
+            "unit": "ops/sec",
+            "extra": "55 samples"
           }
         ]
       }
