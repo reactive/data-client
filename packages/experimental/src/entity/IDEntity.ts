de401a6cbb82a5cb4b9f911a464bb081c319de29
Nathaniel Tucker <me@ntucker.me>
2021-05-16 16:11:27 -0700

    feat: New Entity that normalizes to pojos (#821)

---
+++ packages/experimental/src/entity/IDEntity.ts
@@ -0,0 +1,16 @@
+import Entity from './Entity';
+
+/** Represents data with primary key being from 'id' field. */
+export default class IDEntity extends Entity {
+  readonly id: string | number | undefined = undefined;
+
+  /**
+   * A unique identifier for each Entity
+   *
+   * @param [parent] When normalizing, the object which included the entity
+   * @param [key] When normalizing, the key where this entity was found
+   */
+  pk(parent?: any, key?: string): string | undefined {
+    return `${this.id}`;
+  }
+}
--- packages/experimental/src/index.ts
+++ packages/experimental/src/index.ts
@@ -1,2 +1,4 @@
 export { default as useFetcher } from './useFetcher';
 export { default as createFetch } from './createFetch';
+export { default as Entity } from './entity/Entity';
+export { default as EntityRecord } from './entity/EntityRecord';
