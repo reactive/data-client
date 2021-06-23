4e24052672b052c612295ab49b7651fbfe8dfb06
Nathaniel Tucker <me@ntucker.me>
2021-06-23 17:35:38 -0500

    fix: Legacy cjs and unpkg should target 'legacy'

--- packages/legacy/rollup.config.js
+++ packages/legacy/rollup.config.js
@@ -14,6 +14,7 @@ const dependencies = Object.keys(pkg.peerDependencies).filter(
 
 const extensions = ['.js', '.ts', '.tsx', '.mjs', '.json', '.node'];
 process.env.NODE_ENV = 'production';
+process.env.BROWSERSLIST_ENV = 'legacy';
 
 function isExternal(id) {
   const ret = dependencies.includes(id);
