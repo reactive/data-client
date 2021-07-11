467b736244a989fedac3e1fbe89982cc5576bbe4
Nathaniel Tucker <me@ntucker.me>
2021-07-11 12:27:41 -0500

    fix: Todo example

--- examples/todo-app/webpack.config.js
+++ examples/todo-app/webpack.config.js
@@ -1,4 +1,5 @@
 const { makeConfig } = require('@anansi/webpack-config');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 const options = {
   basePath: 'src',
@@ -9,12 +10,15 @@ const options = {
     template: 'index.ejs',
   },
   globalStyleDir: 'style',
+  libraryInclude: /node_modules\/(@rest-hooks\/)/,
+  libraryExclude: /node_modules(?!\/(@rest-hooks\/))/,
 };
 
 const generateConfig = makeConfig(options);
 
 module.exports = (env, argv) => {
   const config = generateConfig(env, argv);
+  config.resolve.plugins.push(new TsconfigPathsPlugin());
   return config;
 };
 
