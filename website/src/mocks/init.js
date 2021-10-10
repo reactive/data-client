0e203b9f74757003c49d6026d15147bfad82a1f8
Nathaniel Tucker <me@ntucker.me>
2021-10-10 15:57:36 -0500

    docs(fix): Site compiles server side (#1360)

--- website/src/mocks/init.js
+++ website/src/mocks/init.js
@@ -1,3 +1,7 @@
-const { worker } = require('./browser');
-
-worker.start();
+if (typeof window !== 'undefined') {
+  const { worker } = require('./browser');
+  worker.start();
+} else {
+  const { server } = require('./server');
+  server.listen();
+}
