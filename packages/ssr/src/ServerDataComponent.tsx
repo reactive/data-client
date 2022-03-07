10a77f05687f42cca7e8bd675c95f69b8c915410
Nathaniel Tucker <me@ntucker.me>
2022-03-06 23:56:02 -0600

    feat: Add @rest-hooks/ssr (#1767)
    Co-authored-by: Jordan Harband <ljharb@gmail.com>

---
+++ packages/ssr/src/ServerDataComponent.tsx
@@ -0,0 +1,21 @@
+import { initialState } from '@rest-hooks/core';
+import type { State } from '@rest-hooks/core';
+
+export const ServerDataComponent = ({ data }: { data: State<unknown> }) => {
+  return (
+    <script
+      type="application/json"
+      id="server-data"
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify(data),
+      }}
+    />
+  );
+};
+
+export function getDatafromDOM(): State<unknown> {
+  const element = document.querySelector(`#server-data`);
+  return element?.innerHTML ? JSON.parse(element?.innerHTML) : initialState;
+}
+
+export default ServerDataComponent;
---
+++ packages/ssr/src/index.ts
@@ -0,0 +1,2 @@
+export * from '@rest-hooks/ssr/ServerDataComponent';
+export { default as createPersistedStore } from '@rest-hooks/ssr/createPersistedStore';
