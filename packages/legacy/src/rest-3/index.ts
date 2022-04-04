f631710b147ff49b15748597d6c7decdad1b3755
Nathaniel Tucker <me@ntucker.me>
2022-04-04 00:31:20 -0500

    feat: Add @rest-hooks/rest@3

---
+++ packages/legacy/src/rest-3/index.ts
@@ -0,0 +1,14 @@
+import Resource from '@rest-hooks/legacy/rest-3/Resource';
+import SimpleResource from '@rest-hooks/legacy/rest-3/SimpleResource';
+
+export { Resource, SimpleResource };
+export { default as EntityRecord } from '@rest-hooks/legacy/rest-3/EntityRecord';
+
+export type {
+  RestEndpoint,
+  RestFetch,
+  FetchMutate,
+  FetchGet,
+} from '@rest-hooks/legacy/rest-3/types';
+
+export * from '@rest-hooks/endpoint';
---
+++ packages/legacy/src/rest-3/paramsToString.ts
@@ -0,0 +1,7 @@
+export default function paramsToString(
+  searchParams: Readonly<Record<string, string | number | boolean>>,
+) {
+  const params = new URLSearchParams(searchParams as any);
+  params.sort();
+  return params.toString();
+}
