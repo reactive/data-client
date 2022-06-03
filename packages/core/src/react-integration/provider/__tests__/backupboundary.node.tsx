e8aff22fe754bf47691b8f6c1b27d45335445def
Nathaniel Tucker <me@ntucker.me>
2022-06-03 15:29:26 -0500

    fix: Hydration mismatch in React 17 (#2039)

---
+++ packages/core/src/react-integration/provider/__tests__/backupboundary.node.tsx
@@ -0,0 +1,33 @@
+// eslint-env jest
+import React, { version } from 'react';
+import { renderToString } from 'react-dom/server';
+
+import BackupBoundary from '../BackupBoundary';
+
+describe('<BackupBoundary />', () => {
+  let warnspy: jest.SpyInstance;
+  beforeEach(() => {
+    warnspy = jest.spyOn(global.console, 'warn');
+  });
+  afterEach(() => {
+    warnspy.mockRestore();
+  });
+
+  it('should warn users about missing Suspense', () => {
+    const tree = (
+      <BackupBoundary>
+        <div>hi</div>
+      </BackupBoundary>
+    );
+    const LegacyReact = version.startsWith('16') || version.startsWith('17');
+    const msg = renderToString(tree);
+    if (LegacyReact) {
+      expect(msg).toBeDefined();
+      expect(msg).toMatchInlineSnapshot(
+        `"<div id=\\"rest-hooks-SSR\\"><div>hi</div></div>"`,
+      );
+    } else {
+      expect(msg).toMatchInlineSnapshot(`"<!--$--><div>hi</div><!--/$-->"`);
+    }
+  });
+});
