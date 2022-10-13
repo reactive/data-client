d78b4a7c6566108d404d3ea9d2184fd89b7688f6
Nathaniel Tucker <me@ntucker.me>
2022-10-12 19:24:37 -0500

    docs: Codesplit live editor scope

---
+++ website/src/components/Playground/PreviewWithScope.tsx
@@ -0,0 +1,126 @@
+import { LiveProvider } from 'react-live';
+import React, { useEffect, useState, memo } from 'react';
+import * as restHooks from 'rest-hooks';
+import * as rest from '@rest-hooks/rest';
+import * as hooks from '@rest-hooks/hooks';
+import * as graphql from '@rest-hooks/graphql';
+import BigNumber from 'bignumber.js';
+import type { FixtureEndpoint } from '@rest-hooks/test';
+
+import {
+  TodoResource as BaseTodoResource,
+  Todo,
+  TodoEndpoint,
+} from './resources/TodoResource';
+import ResetableErrorBoundary from '../ResettableErrorBoundary';
+import PreviewWithHeader from './PreviewWithHeader';
+import { babelTransform } from './babelTransform';
+
+function randomFloatInRange(min, max, decimals) {
+  const str = (Math.random() * (max - min) + min).toFixed(decimals);
+
+  return parseFloat(str);
+}
+
+const mockFetch = (getResponse, name, delay = 150) => {
+  const fetch = (...args) =>
+    new Promise(resolve =>
+      setTimeout(() => resolve(getResponse(...args)), delay),
+    );
+  if (name)
+    Object.defineProperty(fetch, 'name', { value: name, writable: false });
+  return fetch;
+};
+
+const mockLastUpdated = ({ id }) => {
+  return new Promise(resolve => {
+    setTimeout(
+      () =>
+        resolve({
+          id,
+          updatedAt: new Date().toISOString(),
+        }),
+      150,
+    );
+  });
+};
+class TimedEntity extends rest.Entity {
+  id = '';
+  pk() {
+    return this.id;
+  }
+
+  static schema = {
+    updatedAt: Date,
+  };
+}
+
+const lastUpdated = new restHooks.Endpoint(mockLastUpdated, {
+  schema: TimedEntity,
+});
+
+function CurrentTime() {
+  const [time, setTime] = useState(() => new Date());
+  useEffect(() => {
+    const intervalID = setInterval(() => setTime(new Date()));
+    return () => clearInterval(intervalID);
+  }, []);
+  return (
+    <time>
+      {Intl.DateTimeFormat('en-US', { timeStyle: 'long' }).format(time)}
+    </time>
+  );
+}
+
+const TodoResource = {
+  ...BaseTodoResource,
+  getList: BaseTodoResource.getList.extend({
+    process(todos) {
+      return todos.slice(0, 7);
+    },
+  }),
+};
+
+const scope = {
+  ...restHooks,
+  ...rest,
+  ...graphql,
+  ...hooks,
+  randomFloatInRange,
+  mockFetch,
+  BigNumber,
+  lastUpdated,
+  TimedEntity,
+  CurrentTime,
+  ResetableErrorBoundary,
+};
+const scopeWithEndpoint = {
+  ...scope,
+  Todo,
+  TodoResource,
+  TodoEndpoint,
+};
+
+export default function PreviewWithScope({
+  code,
+  includeEndpoints,
+  ...props
+}: {
+  code: string;
+  includeEndpoints: boolean;
+  groupId: string;
+  defaultOpen: 'y' | 'n';
+  row: boolean;
+  fixtures: FixtureEndpoint[];
+}) {
+  return (
+    <LiveProvider
+      code={code}
+      transformCode={babelTransform}
+      noInline
+      scope={includeEndpoints ? scopeWithEndpoint : scope}
+    >
+      <PreviewWithHeader {...props} />
+    </LiveProvider>
+  );
+}
