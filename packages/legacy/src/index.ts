264203a53299f1357697156f718300af42c1170d
Nathaniel Tucker <me@ntucker.me>
2023-01-12 11:32:03 -0600

    enhance: Removing ancient shape based hooks
    BREAKING CHANGE: useFetcher, useFetchDispatcher, useInvalidator, useInvalidateDispatcher removed
    
    These predate @rest-hooks/endpoint and @rest-hooks/rest. Probably easier
    to rewrite in modern rest hooks than attempt upgrades with those at this
    point.

--- packages/legacy/src/index.ts
+++ packages/legacy/src/index.ts
@@ -7,4 +7,3 @@ export * from './resource/index.js';
 export { default as useStatefulResource } from './useStatefulResource.js';
 export * from './endpoint/index.js';
 export * as rest3 from './rest-3/index.js';
-export * from './hooks/index.js';
