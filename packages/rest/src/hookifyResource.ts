6d388afcb178a3d139c807e8b3163bcea371f86c
Nathaniel Tucker <me@ntucker.me>
2023-01-15 13:43:38 -0600

    feat: More fleixble types for hookableResource

--- packages/rest/src/hookifyResource.ts
+++ packages/rest/src/hookifyResource.ts
@@ -9,25 +9,27 @@ export interface HookableEndpointInterface extends EndpointInterface {
  *
  * @see https://resthooks.io/rest/api/hookifyResource
  */
-export default function hookifyResource<
-  R extends Record<string, HookableEndpointInterface>,
->(resource: R, useRequestInit: () => RequestInit): HookResource<R> {
+export default function hookifyResource<R extends {}>(
+  resource: R,
+  useRequestInit: () => RequestInit,
+): HookResource<R> {
   const usingResource: Record<string, () => HookableEndpointInterface> = {};
   Object.keys(resource).forEach(key => {
-    usingResource[`use${capitalizeFirstLetter(key)}`] = () => {
-      // this is false positive due to the dynamic nature of assignment
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      const requestInit = useRequestInit();
-      return resource[key].extend({ requestInit });
-    };
+    const endpoint = (resource as any)[key];
+    if (endpoint.extend !== undefined)
+      usingResource[`use${capitalizeFirstLetter(key)}`] = () => {
+        // this is false positive due to the dynamic nature of assignment
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const requestInit = useRequestInit();
+        return endpoint.extend({ requestInit });
+      };
   });
   return usingResource as any;
 }
 
-export type HookResource<R extends Record<string, HookableEndpointInterface>> =
-  {
-    [K in Extract<keyof R, string> as `use${Capitalize<K>}`]: () => R[K];
-  };
+export type HookResource<R extends {}> = {
+  [K in Extract<keyof R, string> as `use${Capitalize<K>}`]: () => R[K];
+};
 
 function capitalizeFirstLetter(s: string) {
   return s.charAt(0).toUpperCase() + s.slice(1);
