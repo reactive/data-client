5cebe8be96d17e5799e3a2298ebf6516e7aacdc3
Nathaniel Tucker <me@ntucker.me>
2023-02-09 11:40:31 -0600

    internal: Run benchmark when bench dir changes

--- examples/benchmark/index.js
+++ examples/benchmark/index.js
@@ -2,13 +2,13 @@ import Benchmark from 'benchmark';
 import vm from 'node:vm';
 import v8 from 'v8';
 
+import addCoreSuite from './core.js';
 import addNormlizrSuite from './normalizr.js';
-import addReducerSuite from './reducer.js';
 
 v8.setFlagsFromString('--expose_gc');
 const gc = vm.runInNewContext('gc');
 
-addReducerSuite(addNormlizrSuite(new Benchmark.Suite()))
+addCoreSuite(addNormlizrSuite(new Benchmark.Suite()))
   .on('cycle', event => {
     // Output benchmark result by converting benchmark result to string
     console.log(String(event.target));
