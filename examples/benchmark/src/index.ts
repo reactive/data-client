ff0f8f93fff820ab12868259f75b81ff44d6267c
Nathaniel Tucker <me@ntucker.me>
2023-02-11 11:51:51 -0800

    internal: Simplify benchmark for github User (#2433)

--- examples/benchmark/src/index.ts
+++ examples/benchmark/src/index.ts
@@ -6,4 +6,3 @@ export {
 } from '@rest-hooks/normalizr';
 export * from '@rest-hooks/core';
 export { Endpoint, Entity, schema, Query } from '@rest-hooks/endpoint';
-export { User, default as UserResource } from './User';
