564348060e407961c2aeb249b977b6912bcce2bf
Nathaniel Tucker <me@ntucker.me>
2023-04-03 11:48:01 -0500

    internal: Clean up some extra config files (#2557)

--- packages/react/rollup.config.js
+++ packages/react/rollup.config.js
@@ -7,7 +7,7 @@ import replace from 'rollup-plugin-replace';
 import { terser } from 'rollup-plugin-terser';
 
 import pkg from './package.json';
-import { typeConfig, typeConfigNext } from '../../rollup-utils';
+import { typeConfig, typeConfigNext } from '../../config/rollup-utils';
 
 const dependencies = Object.keys(pkg.dependencies)
   .concat(Object.keys(pkg.peerDependencies))
