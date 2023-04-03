896e4589af3933e8188d1b8f72989f95b2fe875b
Nathaniel Tucker <me@ntucker.me>
2023-04-03 15:07:46 -0500

    fix: Typing of legacy resource

--- packages/legacy/src/resource/Resource.ts
+++ packages/legacy/src/resource/Resource.ts
@@ -43,7 +43,7 @@ export default abstract class Resource extends SimpleResource {
     };
     options = this.getFetchInit(options);
     if (body && isPojo(body)) options.body = JSON.stringify(body);
-    else if (body) options.body = body;
+    else if (body) options.body = body as BodyInit;
     if (!options.body || typeof options.body === 'string') {
       options.headers = {
         'Content-Type': 'application/json',
