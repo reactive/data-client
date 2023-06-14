001e27adadf99b272bcdeeea3b8598ea62223c0c
Nathaniel Tucker <me@ntucker.me>
2023-06-14 09:30:21 -0500

    feat: Export initialState and createReducer (#2665)

--- packages/redux/src/index.ts
+++ packages/redux/src/index.ts
@@ -1,7 +1,7 @@
+export { applyManager, initialState, createReducer } from '@rest-hooks/core';
 export * from '@rest-hooks/react';
 export { prepareStore } from './prepareStore.js';
 export { default as CacheProvider } from './CacheProvider.js';
 export { default as ExternalCacheProvider } from './ExternalCacheProvider.js';
 export { default as mapMiddleware } from './mapMiddleware.js';
 export { default as PromiseifyMiddleware } from './PromiseifyMiddleware.js';
-export { applyManager } from '@rest-hooks/core';
