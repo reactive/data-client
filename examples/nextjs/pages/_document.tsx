a6ae2b35f90bfa7556f94f5940b84640010523cc
Nathaniel Tucker <me@ntucker.me>
2023-07-03 11:30:17 -0500

    docs(fix): nextjs example

--- examples/nextjs/pages/_document.tsx
+++ examples/nextjs/pages/_document.tsx
@@ -1,7 +1,7 @@
-import { RestHooksDocument } from '@data-client/ssr/nextjs';
+import { DataClientDocument } from '@data-client/ssr/nextjs';
 import type { DocumentContext } from 'next/document.js';
 
-export default class MyDocument extends RestHooksDocument {
+export default class MyDocument extends DataClientDocument {
   static getNonce(ctx: DocumentContext & { res: { nonce?: string } }) {
     // this assumes nonce has been added here - customize as you need
     return ctx?.res?.nonce;
