cd3e682cd226ee3f4afad5d3524927dde69abbb9
Nathaniel Tucker <me@ntucker.me>
2023-08-04 16:50:44 -0500

    docs: More consistent background coloring for playground

--- website/src/components/Playground/usingMonaco.ts
+++ website/src/components/Playground/usingMonaco.ts
@@ -1,4 +1,5 @@
 const usingMonaco =
+  // React 18: perhaps this causes hydration problems.
   typeof window !== 'undefined' &&
   // crawler or mobile browser
   !/bot|googlebot|crawler|spider|robot|crawling|Mobile|Android|BlackBerry/i.test(
