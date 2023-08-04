28bf27536778d3fac370204936a8b113aa6f0211
Nathaniel Tucker <me@ntucker.me>
2023-08-04 17:04:59 -0500

    docs: Try to get rid of code background flash when in light mode

--- website/src/components/Playground/useReactLiveTheme.tsx
+++ website/src/components/Playground/useReactLiveTheme.tsx
@@ -1,17 +1,13 @@
-import { useColorMode, usePrismTheme } from '@docusaurus/theme-common';
+import { usePrismTheme } from '@docusaurus/theme-common';
 import { useMemo } from 'react';
 
 export function useReactLiveTheme() {
   const prismTheme = usePrismTheme();
-  const { colorMode } = useColorMode();
   const realTheme = useMemo(() => {
-    if (colorMode === 'dark') {
-      return {
-        ...prismTheme,
-        plain: { ...prismTheme.plain, backgroundColor: 'hsl(229, 20%, 15%)' },
-      };
-    }
-    return prismTheme;
-  }, [colorMode, prismTheme]);
+    return {
+      ...prismTheme,
+      plain: { ...prismTheme.plain, backgroundColor: 'transparent' },
+    };
+  }, [prismTheme]);
   return realTheme;
 }
