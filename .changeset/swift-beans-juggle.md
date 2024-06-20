47bf5aff1a473f113b14b9c619e838e766c394ce
Nathaniel Tucker <me@ntucker.me>
2024-06-20 15:37:34 +0200

    fix(native): Render nothing for dev button

---
+++ .changeset/swift-beans-juggle.md
@@ -0,0 +1,5 @@
+---
+'@data-client/react': patch
+---
+
+Fix React Native should not render anything for dev button
---
+++ packages/react/src/components/renderDevButton.native.tsx
@@ -0,0 +1,9 @@
+import type { DevToolsPosition } from './DevToolsButton.js';
+
+export function renderDevButton(
+  devButton: DevToolsPosition | null | undefined,
+  hasDevManager: boolean,
+) {
+  /* istanbul ignore next */
+  return null;
+}
