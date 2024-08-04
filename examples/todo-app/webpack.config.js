0ad4f5ddbf34d2b6a8c31ddd4f3804c02ff35df4
Nathaniel Tucker <me@ntucker.me>
2024-08-04 15:12:12 +0200

    docs(demo): Add codesandbox link for todo

--- examples/todo-app/webpack.config.js
+++ examples/todo-app/webpack.config.js
@@ -17,6 +17,9 @@ const generateConfig = makeConfig(options);
 module.exports = (env, argv) => {
   const config = generateConfig(env, argv);
   config.entry = config.entry.App;
+  if (config.devServer) {
+    config.devServer.allowedHosts = ['.csb.app', 'localhost', '127.0.0.1'];
+  }
   return config;
 };
 
