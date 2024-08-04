d51862dd4f356598ca3adaac83696aafa8488b2d
Nathaniel Tucker <me@ntucker.me>
2024-08-04 16:54:11 +0200

    docs: Homepage demo code spacing

--- website/src/components/Demo/code/profile-edit/graphql/ProfileEdit.tsx
+++ website/src/components/Demo/code/profile-edit/graphql/ProfileEdit.tsx
@@ -2,6 +2,7 @@ import { UserResource } from './resources';
 
 export default function ProfileEdit({ id }: { id: number }) {
   const { user } = useSuspense(UserResource.get, { id });
+
   const controller = useController();
   const handleChange = ({ currentTarget: { value: name } }) =>
     controller.fetch(UserResource.update, { id, name });
