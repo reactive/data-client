285d293f84812d6b7ab144fbfc7c974c7817fce5
Nathaniel Tucker <me@ntucker.me>
2024-09-29 16:07:28 +0100

    docs(demo): Reset error boundary on navigation

--- examples/github-app/src/Boundary.tsx
+++ examples/github-app/src/Boundary.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from '@anansi/router';
 import { AsyncBoundary } from '@data-client/react';
 import { styled } from '@linaria/react';
 import { Spin } from 'antd';
@@ -11,7 +12,13 @@ function Boundary({
   children: ReactNode;
   fallback?: ReactNode;
 }) {
-  return <AsyncBoundary fallback={fallback}>{children}</AsyncBoundary>;
+  const router = useRouter();
+
+  return (
+    <AsyncBoundary fallback={fallback} listen={router.history.listen}>
+      {children}
+    </AsyncBoundary>
+  );
 }
 export default memo(Boundary);
 
