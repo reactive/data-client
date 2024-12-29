57e0b99a0dc8fbca08e1983a31f55334a6aa4655
Nathaniel Tucker <me@ntucker.me>
2024-12-29 15:15:44 +0000

    internal: Remove ssr package

--- packages/ssr/src/nextjs/index.ts
+++
@@ -1,9 +0,0 @@
-Object.hasOwn =
-  Object.hasOwn ||
-  /* istanbul ignore next */ function hasOwn(it, key) {
-    return Object.prototype.hasOwnProperty.call(it, key);
-  };
-export { default as DataClientDocument } from './DataClientDocument.js';
-export { default as DataProvider } from './DataProvider/DataProvider.js';
-export { default as AppDataProvider } from './AppDataProvider.js';
-export { default as AppCacheProvider } from './AppDataProvider.js';
