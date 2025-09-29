a7b3be8aa5f46ab2d523639391df172d6614fe39
Nathaniel Tucker <me@ntucker.me>
2025-09-28 23:52:13 -0400

    feat: useSubscription

--- packages/vue/src/consumers/index.ts
+++ packages/vue/src/consumers/index.ts
@@ -1,4 +1,5 @@
 export { default as useSuspense } from './useSuspense.js';
+export { default as useSubscription } from './useSubscription.js';
 export {
   injectController as useController,
   injectState as useStateRef,
