38396279f53fcf175e635c9833f09affde5553ab
Nathaniel Tucker <me@ntucker.me>
2025-09-30 23:08:25 -0400

    feat: Add more composables

--- packages/vue/src/consumers/index.ts
+++ packages/vue/src/consumers/index.ts
@@ -1,4 +1,7 @@
 export { default as useSuspense } from './useSuspense.js';
 export { default as useSubscription } from './useSubscription.js';
 export { default as useQuery } from './useQuery.js';
+export { default as useLive } from './useLive.js';
+export { default as useLoading } from './useLoading.js';
+export { default as useDebounce } from './useDebounce.js';
 export { useController as useController } from '../context.js';
---
+++ packages/vue/src/consumers/useLive.ts
@@ -0,0 +1,64 @@
+import type {
+  EndpointInterface,
+  Denormalize,
+  Schema,
+  FetchFunction,
+  ResolveType,
+  DenormalizeNullable,
+} from '@data-client/core';
+import type { DeepReadonly, ComputedRef } from 'vue';
+
+import useSubscription from './useSubscription.js';
+import useSuspense from './useSuspense.js';
+
+/**
+ * Ensure an endpoint is available. Keeps it fresh once it is.
+ *
+ * useSuspense() + useSubscription()
+ * @see https://dataclient.io/docs/api/useLive
+ * @throws {Promise} If data is not yet available.
+ * @throws {NetworkError} If fetch fails.
+ */
+export default function useLive<
+  E extends EndpointInterface<
+    FetchFunction,
+    Schema | undefined,
+    undefined | false
+  >,
+>(
+  endpoint: E,
+  ...args: readonly [...Parameters<E>]
+): Promise<
+  DeepReadonly<
+    ComputedRef<
+      E['schema'] extends undefined | null ? ResolveType<E>
+      : Denormalize<E['schema']>
+    >
+  >
+>;
+
+export default function useLive<
+  E extends EndpointInterface<
+    FetchFunction,
+    Schema | undefined,
+    undefined | false
+  >,
+>(
+  endpoint: E,
+  ...args: readonly [...Parameters<E>] | readonly [null]
+): Promise<
+  DeepReadonly<
+    ComputedRef<
+      E['schema'] extends undefined | null ? ResolveType<E> | undefined
+      : DenormalizeNullable<E['schema']>
+    >
+  >
+>;
+
+export default async function useLive(
+  endpoint: any,
+  ...args: any[]
+): Promise<any> {
+  useSubscription(endpoint, ...args);
+  return useSuspense(endpoint, ...args);
+}
