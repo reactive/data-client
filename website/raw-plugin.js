59845ba8fb114fd2cc8aa161c6e48803d8ba1a5a
Nathaniel Tucker <me@ntucker.me>
2026-01-02 10:33:48 -0500

    docs: Add 0.10 release post

--- website/raw-plugin.js
+++ website/raw-plugin.js
@@ -4,6 +4,15 @@ module.exports = function (context, options) {
     // to support running babel transformer we need to polyfill node api 'fs'
     configureWebpack(config, isServer, utils) {
       return {
+        ignoreWarnings: [
+          // Suppress warning about dynamic import expressions in monaco-init.ts
+          // This is expected behavior for dynamic TypeScript definition loading
+          {
+            module: /monaco-init\.ts/,
+            message:
+              /Critical dependency: the request of a dependency is an expression/,
+          },
+        ],
         module: {
           rules: [
             {
